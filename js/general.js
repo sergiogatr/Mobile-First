e8557c77160112208cb0c334e8d11fe1254b2e6c
Sergio García <71490024+sergiogatr@users.noreply.github.com>
2022-02-08 14:48:07 +0100

    commit

--- js/general.js
+++ js/general.js
@@ -1,7 +1,7 @@
-function calcular() {
-    var ventana_ancho = $(window).width();
-    var ventana_alto = $(window).height();
-    $("h4").text("Viewport dimensions " + ventana_ancho + "x" + ventana_alto);   
-}
+// function calcular() {
+//     var ventana_ancho = $(window).width();
+//     var ventana_alto = $(window).height();
+//     $("h4").text("Viewport dimensions " + ventana_ancho + "x" + ventana_alto);   
+// }
 
-setInterval(calcular, 0);
+// setInterval(calcular, 0);
